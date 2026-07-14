cca16053b6c7ca07e126b5057179c7eba175efd8
Subha Sundar Das <mail.subhasundardas@gmail.com>
2026-07-14 18:13:35 +0530

    Add example (windows)

--- example/window.go
+++ example/window.go
@@ -3,19 +3,135 @@ package example
 import (
 	"github.com/subhasundardass/retui/retui"
 	"github.com/subhasundardass/retui/retui/components"
+	"github.com/subhasundardass/retui/retui/window"
 )
 
 func WindowsExample(props retui.Props) retui.Element {
+
+	// Handle keyboard shortcuts
+	handleKeyboard()
+
 	return retui.Box(
 		props,
 		retui.NewStyle(),
 		components.Panel(
 			"Windows", 100,
 
-			retui.Text("Window 1", retui.NewStyle().Foreground(retui.BrightCyan).Bold(true)),
-			retui.Text("Window 2", retui.NewStyle().Foreground(retui.BrightCyan).Bold(true)),
-			retui.Text("Window 3", retui.NewStyle().Foreground(retui.BrightCyan).Bold(true)),
-			retui.Text("Modal Window", retui.NewStyle().Foreground(retui.BrightCyan).Bold(true)),
+			retui.Text("1 - Simple window 1", retui.NewStyle()),
+			retui.Text("2 - Simple Window 2", retui.NewStyle()),
+			retui.Text("3 - Open modal Window", retui.NewStyle()),
+			retui.Text("c - Close all windows", retui.NewStyle()),
+			retui.Text("ESC - Close focused window", retui.NewStyle()),
 		),
 	)
+}
+
+func handleKeyboard() {
+
+	switch retui.CurrentKey.Rune {
+	case '1':
+		w := window1()
+		w.Show()
+		// w.Focus()
+
+	case '2':
+		w := window2()
+		w.Show()
+		// w.Focus()
+
+	case '3':
+		w := modalWindow()
+		w.Show()
+		// w.Focus()
+
+	case 'c':
+		window.CloseAll()
+	case 'q':
+		retui.Exit()
+	}
+
+}
+
+func window1() *window.Window {
+
+	content := retui.Box(
+		retui.Props{
+			Height: retui.Fixed(5),
+			Width:  retui.Grow(1),
+		},
+		retui.NewStyle(),
+		retui.Text("Window 1", retui.NewStyle()),
+	)
+
+	win := window.NewWindow().
+		SetTitle("Simple Window").
+		SetModal(false).
+		SetContent(content).
+		SetPosition(20, 10)
+
+	win.OnKeyPress(func(key retui.Key) {
+		if key.Code == retui.KeyEscape {
+			retui.Infof("Modal received key: %+v", key)
+			win.Close()
+		}
+	})
+
+	return win
+}
+
+func window2() *window.Window {
+
+	content := components.Panel(
+		"Window 1", 40,
+		retui.Box(
+			retui.Props{
+				Height: retui.Fixed(4),
+				Width:  retui.Grow(1),
+			},
+			retui.NewStyle(),
+			retui.Text("Window 2", retui.NewStyle()),
+		),
+	)
+
+	win := window.NewWindow().
+		SetTitle("Simple Window").
+		SetModal(false).
+		SetContent(content).
+		SetPosition(30, 20)
+
+	win.OnKeyPress(func(key retui.Key) {
+		if key.Code == retui.KeyEscape {
+			retui.Infof("Modal received key: %+v", key)
+			win.Close()
+		}
+	})
+
+	return win
+}
+
+func modalWindow() *window.Window {
+
+	content := retui.Box(
+		retui.Props{
+			Height: retui.Fixed(5),
+			Width:  retui.Grow(1),
+		},
+		retui.NewStyle(),
+		retui.Text("Modal Window", retui.NewStyle()),
+	)
+
+	win := window.NewWindow().
+		SetTitle("Simple Window").
+		SetModal(true).
+		SetContent(content).
+		SetPosition(10, 20)
+
+	win.OnKeyPress(func(key retui.Key) {
+		if key.Code == retui.KeyEscape {
+			retui.Infof("Modal received key: %+v", key)
+			win.Close()
+		}
+	})
+
+	return win
 }
